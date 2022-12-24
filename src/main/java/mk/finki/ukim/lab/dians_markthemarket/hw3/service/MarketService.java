c6a7365cbbeaa97ca4d648db93b73a1d8a2459b4
nenadpetrovikj <86932367+nenadpetrovikj@users.noreply.github.com>
2022-12-24 18:09:32 +0100

    added files to service package. few small changes.

---
+++ src/main/java/mk/finki/ukim/lab/dians_markthemarket/hw3/service/MarketService.java
@@ -0,0 +1,10 @@
+package mk.finki.ukim.lab.dians_markthemarket.hw3.service;
+
+import mk.finki.ukim.lab.dians_markthemarket.hw3.model.Market;
+
+import java.util.List;
+
+public interface MarketService {
+
+    List<Market> findAllMarkets();
+}
--- src/main/java/mk/finki/ukim/lab/dians_markthemarket/hw3/web/MarketController.java
+++ src/main/java/mk/finki/ukim/lab/dians_markthemarket/hw3/web/MarketController.java
@@ -1,8 +1,11 @@
 package mk.finki.ukim.lab.dians_markthemarket.hw3.web;
 
 import mk.finki.ukim.lab.dians_markthemarket.hw3.service.MarkTheMarketInitDatabaseService;
+import mk.finki.ukim.lab.dians_markthemarket.hw3.service.MarketService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.FileNotFoundException;
@@ -12,14 +15,22 @@ import java.io.FileNotFoundException;
 public class MarketController {
 
     private final MarkTheMarketInitDatabaseService markTheMarketInitDatabaseService;
+    private final MarketService marketService;
 
-    public MarketController(MarkTheMarketInitDatabaseService markTheMarketInitDatabaseService) {
+    public MarketController(MarkTheMarketInitDatabaseService markTheMarketInitDatabaseService, MarketService marketService) {
         this.markTheMarketInitDatabaseService = markTheMarketInitDatabaseService;
+        this.marketService = marketService;
     }
 
     @GetMapping
     public String getMarketPage() throws FileNotFoundException {
         markTheMarketInitDatabaseService.createFinal();
+        return "index";
+    }
+
+    @PostMapping("/findAllMarkets")
+    public String findAllMarkets(Model model) {
+        model.addAttribute("markets", marketService.findAllMarkets());
         return "index";
     }
 }
